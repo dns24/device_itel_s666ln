ce6e6c265fe2f0f0792520eb89bf700c0e816250
dns24 <wahidns24@gmail.com>
2024-12-29 07:57:29 +0000

    S666LN: use another repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,14 +2,14 @@
 git clone -b lineage-21 https://github.com/mt6789-transsion/device_itel_S666LN-kernel device/itel/S666LN-kernel
 
 # Clone Device Vendor Tree
-git clone -b lineage-21 https://github.com/mt6789-transsion/vendor_itel_S666LN vendor/itel/S666LN
+git clone -b lineage-21 https://github.com/dns24/vendor_itel_S666LN vendor/itel/S666LN
 
 # Clone Device Hardware
-git clone -b lineage-20.0 https://github.com/Anything-at-25-00/hardware_transsion hardware/transsion
+git clone -b lineage-21.0 https://github.com/mt6789-transsion/hardware_transsion hardware/transsion
 
 # Clone Device Hardware Mediatek
 rm -rf hardware/mediatek
-git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
 
 # Clone Device Mediatek Sepolicy Vendor
 git clone -b lineage-21 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
